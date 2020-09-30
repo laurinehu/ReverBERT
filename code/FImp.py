839b368c13f520f2d8c0f88f7f8e965158944e69
Authary <contact@alexandrebazin.com>
2020-09-30 11:18:13 +0200

    Update FImp.py

--- code/FImp.py
+++ code/FImp.py
@@ -7,6 +7,7 @@ Created on Wed Sep  9 14:16:23 2020
 
 import copy
 import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
 
 def shuffle(k, X):
     Y = copy.deepcopy(X)
@@ -88,3 +89,29 @@ def feature_importance(data_test, target_test, classifier, n_perm):
             S = S + (accu-accu0)
         R[k] = S
     return R
+
+def optiRF(data,labels):
+    # Number of trees in random forest
+    n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+    # Number of features to consider at every split
+    max_features = ['auto', 'sqrt']
+    # Maximum number of levels in tree
+    max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+    max_depth.append(None)
+    # Minimum number of samples required to split a node
+    min_samples_split = [2, 5, 10]
+    # Minimum number of samples required at each leaf node
+    min_samples_leaf = [1, 2, 4]
+    # Method of selecting samples for training each tree
+    bootstrap = [True, False]# Create the random grid
+    random_grid = {'n_estimators': n_estimators, 'max_features': max_features, 'max_depth': max_depth, 'min_samples_split': min_samples_split, 'min_samples_leaf': min_samples_leaf, 'bootstrap': bootstrap}
+    # Use the random grid to search for best hyperparameters
+    # First create the base model to tune
+    rf = RandomForestRegressor()
+    # Random search of parameters, using 3 fold cross validation, 
+    # search across 100 different combinations, and use all available cores
+    rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+    
+    rf_random.fit(train_features, train_labels)
+    
+    return rf
